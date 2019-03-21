16b7390107891193abf423636fb05ff9132ca59c
songbo <songbo_work@163.com>
2019-03-21 22:54:57 +0800

    ":sparkles: 增加vue-router和顶部tab组件"

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header'
+import Tab from 'components/tab'
 export default {
   components: {
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import fastclick from 'fastclick'
 
 import 'common/scss/index.scss'
@@ -11,6 +11,6 @@ fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   render: h => h(App)
 })
