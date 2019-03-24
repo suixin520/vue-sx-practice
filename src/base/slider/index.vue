e9c989bb92ca712b56233d053aff90292671b292
songbo <songbo_work@163.com>
2019-03-24 23:27:40 +0800

    ":sparkle: 增加推荐页轮播图的实现"

---
+++ src/base/slider/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+    <div class="dots">
+      <span class="dot" :class="{active: currentPageIndex === i}" v-for="(item, i) in dots" :key="i"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+import { addClass } from 'common/js/dom'
+export default {
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0,
+      children: null
+    }
+  },
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 3000
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this._setSliderWidth()
+      this._initDots()
+      this._initSlider()
+
+      if (this.autoPlay) {
+        this._play()
+      }
+    }, 20)
+
+    window.addEventListener('resize', () => {
+      if (!this.slider) {
+        return
+      }
+      this._setSliderWidth(true)
+      this.slider.refresh()
+    })
+  },
+
+  methods: {
+    _setSliderWidth(isResize) {
+      this.children = this.$refs.sliderGroup.children
+
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i]
+        addClass(child, 'slider-item')
+        child.style.width = sliderWidth + 'px'
+        width += sliderWidth
+      }
+
+      if (this.loop && !isResize) {
+        width += 2 * sliderWidth
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initDots() {
+      this.dots = new Array(this.children.length)
+    },
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3,
+          speed: 400
+        }
+      })
+
+      this.slider.on('scrollEnd', () => {
+        let pageIndex = this.slider.currentPage.pageX
+        if (this.loop) {
+          pageIndex -= 1
+        }
+        this.currentPageIndex = pageIndex
+
+        if (this.autoPlay) {
+          this._play()
+        }
+      })
+    },
+    _play() {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.slider.next()
+      }, this.interval)
+    }
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~common/scss/variable";
+
+.slider {
+  min-height: 1px;
+  position: relative;
+  .slider-group {
+    overflow: hidden;
+    white-space: nowrap;
+    .slider-item {
+      float: left;
+      box-sizing: border-box;
+      overflow: hidden;
+      text-align: center;
+      a {
+        display: block;
+        width: 100%;
+        overflow: hidden;
+        text-decoration: none;
+      }
+      img {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
+  .dots {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 12px;
+    text-align: center;
+    font-size: 0;
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background: $color-text-l;
+      &.active {
+        width: 20px;
+        border-radius: 5px;
+        background: $color-text-ll;
+      }
+    }
+  }
+}
+</style>
--- src/components/recommend/index.vue
+++ src/components/recommend/index.vue
@@ -1,24 +1,47 @@
 <template>
-  <div>
-    推荐页面
+  <div class="recommend">
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="(item, i) in recommends" :key="i">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="暂无图片">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import {getRecommend} from 'api/recommend'
 import {SUCCESS} from 'api/config'
+import Slider from 'base/slider'
 export default {
+  data() {
+    return {
+      recommends: []
+    }
+  },
   created() {
     this._getRecommend()
   },
   methods: {
     _getRecommend() {
       getRecommend().then((res) => {
-        if (SUCCESS === res.code) {
-          console.log(res)
+        if (res.code === SUCCESS) {
+          this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
