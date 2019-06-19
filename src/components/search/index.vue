ecddfd3f0edd8d9374253572c71db09531a6a655
songbo <songbo_work@163.com>
2019-06-19 21:16:18 +0800

    ":sparkles: 剩余历史列表滚动，计算底部mini播放器高度的实现"

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -3,30 +3,33 @@
     <div class="search-box-wrapper">
       <search-box ref="searchBox" @query="onQueryChange"></search-box>
     </div>
-    <div class="shortcut-wrapper" v-show="!query">
-      <div class="shortcut">
-        <div class="hot-key">
-          <h1 class="title">热门搜索</h1>
-          <ul>
-            <li @click="addQuery(item.k)" class="item" v-for="(item, i) in hotKey" :key="i">
-              <span>{{item.k}}</span>
-            </li>
-          </ul>
+    <div ref="shortcutWrapper" class="shortcut-wrapper" v-show="!query">
+      <scroll ref="shortcut" class="shortcut" :data="shortcut">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li @click="addQuery(item.k)" class="item" v-for="(item, i) in hotKey" :key="i">
+                <span>{{item.k}}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="search-history" v-show="searchHistory.length">
+            <h1 class="title">
+              <span class="text">搜索历史</span>
+              <span class="clear" @click.stop="showConfirm">
+                <i class="icon-clear"></i>
+              </span>
+            </h1>
+            <search-list :searches="searchHistory" @select="addQuery" @delete="deleteSearchHistory"></search-list>
+          </div>
         </div>
-        <div class="search-history" v-show="searchHistory.length">
-          <h1 class="title">
-            <span class="text">搜索历史</span>
-            <span class="clear" @click.stop="clearSearchHistory">
-              <i class="icon-clear"></i>
-            </span>
-          </h1>
-          <search-list :searches="searchHistory" @select="addQuery" @delete="deleteSearchHistory"></search-list>
-        </div>
-      </div>
+      </scroll>
     </div>
-    <div class="search-result" v-show="query">
-      <suggest @listScroll="blurInput" :query="query" @select="saveSearch"></suggest>
+    <div ref="searchResult" class="search-result" v-show="query">
+      <suggest ref="suggest" @listScroll="blurInput" :query="query" @select="saveSearch"></suggest>
     </div>
+    <confirm ref="confirm" text="是否清空所有搜索历史" @confirm="clearSearchHistory"></confirm>
     <router-view></router-view>
   </div>
 </template>
@@ -38,8 +41,12 @@ import {SUCCESS} from 'api/config'
 import Suggest from '../suggest'
 import {mapActions, mapGetters} from 'vuex'
 import SearchList from 'base/search-list'
+import Confirm from 'base/confirm'
+import Scroll from 'base/scroll'
+import {playlistMixin} from 'common/js/mixin'
 
 export default {
+  mixins: [playlistMixin],
   data() {
     return {
       hotKey: [],
@@ -47,6 +54,9 @@ export default {
     }
   },
   computed: {
+    shortcut() {
+      return this.hotKey.concat(this.searchHistory)
+    },
     ...mapGetters([
       'searchHistory'
     ])
@@ -55,6 +65,13 @@ export default {
     this._getHotKey()
   },
   methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+      this.$refs.shortcutWrapper.style.bottom = bottom
+      this.$refs.shortcut.refresh()
+      this.$refs.searchResult.style.bottom = bottom
+      this.$refs.suggest.refresh()
+    },
     addQuery(k) {
       this.$refs.searchBox.setQuery(k)
     },
@@ -66,6 +83,9 @@ export default {
     },
     saveSearch() {
       this.saveSearchHistory(this.query)
+    },
+    showConfirm() {
+      this.$refs.confirm.show()
     },
     _getHotKey() {
       getHotKey().then((res) => {
@@ -80,10 +100,21 @@ export default {
       'clearSearchHistory'
     ])
   },
+  watch: {
+    query(newQuery) {
+      if (!newQuery) {
+        setTimeout(() => {
+          this.$refs.shortcut.refresh()
+        }, 20)
+      }
+    }
+  },
   components: {
     SearchBox,
     Suggest,
-    SearchList
+    SearchList,
+    Confirm,
+    Scroll
   }
 }
 </script>
